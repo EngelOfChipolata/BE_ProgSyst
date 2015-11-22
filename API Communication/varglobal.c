9882da1f5106f8a438909380c4cc91fc5e2c7e29
EngelOfChipolata <engelofdeath@live.fr>
2015-11-22 19:12:13 +0100

    [Code var global] j'ai appris a initialiser des structures...

--- API Communication/varglobal.c
+++ API Communication/varglobal.c
@@ -1,15 +1,5 @@
 #include "varglobal.h"
+#include "structures.h"
 
-_idgest.idgest = 0;
-_idgest.mutexgest = PTHREAD_MUTEX_INITIALIZER;
-
-_zoneRequete.flag_req = 0;
-_zoneRequete.id_thread = 0;
-_zoneRequete.msg = "";
-_zoneRequete.mutexreq = PTHREAD_MUTEX_INITIALIZER;
-_zoneRequete.numrequest = 0;
-_zoneRequete.repzoneaddr = NULL;
-_zoneRequete.userid1 = 0;
-_zoneRequete.userid2 = 0;
-_zoneRequete.var_cond_req_empty = PTHREAD_COND_INITIALIZER;
-_zoneRequete.var_cond_req_full = PTHREAD_COND_INITIALIZER;
+idThreadGest _idgest = {0, PTHREAD_MUTEX_INITIALIZER};
+requestZone _zoneRequete = {0, 0, 0, NULL, 0, "", 0, PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER};
