69b8120e0b0c0b32148900cdbcba054ab7ba1c9e
EngelOfChipolata <engelofdeath@live.fr>
2015-11-22 19:15:52 +0100

    [Code] Corrige un erreur de syntaxe sur les strucutres

--- API Communication/structures.h
+++ API Communication/structures.h
@@ -4,28 +4,9 @@
 #define TAILLEBAL 10
 
 #include "pthread.h"
-typedef struct requestZone;
-typedef struct repZone;
-typedef struct BaL;
-typedef struct Annuaire;
-typedef struct idThreadGest;
 
 
-struct requestZone{
-    int numrequest;
-    int userid1;
-    int userid2;
-    answerZone * repzoneaddr;
-    pthread_t id_thread;
-    char * msg[TAILLEMAX];
-    int flag_req;
-    pthread_mutex_t mutexreq;
-    pthread_cond_t var_cond_req_full;
-    pthread_cond_t var_cond_req_empty;
-};
-
-
-struct repZone{
+typedef struct repzone{
     int flag_rep;
     int code_err;
     union{
@@ -35,10 +16,23 @@ struct repZone{
     pthread_mutex_t mutexrep;
     pthread_cond_t var_cond_rep_full;
     pthread_cond_t var_cond_rep_empty;
-};
+} repZone;
+
+typedef struct requestzone{
+    int numrequest; //1 : abonnement, 2 : écriture, 3: lecture, 4: consultation, 5: desabonnement, 6: findouce
+    int userid1;
+    int userid2;
+    repZone * repzoneaddr;
+    pthread_t id_thread;
+    char msg[TAILLEMAX];
+    int flag_req; //0: pas de requêtes (on peut écrire), 1: requête en attente (on peut lire)
+    pthread_mutex_t mutexreq;
+    pthread_cond_t var_cond_req_full;
+    pthread_cond_t var_cond_req_empty;
+}requestZone;
 
 
-struct BaL{
+typedef struct bal{
     char ** msgs[TAILLEBAL][TAILLEMAX];
     int iecriture;
     int ilecture;
@@ -46,20 +40,20 @@ struct BaL{
     pthread_mutex_t mutex_bal;
     pthread_cond_t var_cond_bal_full;
     pthread_cond_t var_cond_bal_empty;
-};
+}BaL;
 
 
-struct Annuaire{
+typedef struct annuaire{
     int id;
     pthread_t idThread;
     BaL * bal;
     repZone * rep;
-};
+}Annuaire;
 
-struct idThreadGest{
+typedef struct idthreadgest{
     pthread_mutex_t mutexgest;
     pthread_t idgest;
-};
+}idThreadGest;
 
 
 
